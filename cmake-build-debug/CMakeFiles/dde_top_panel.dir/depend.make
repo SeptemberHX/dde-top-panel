f7af5076b5cc700aa6e0cf3c04039661aa22481c
SeptemberHX <september_hx@outlook.com>
2020-05-27 15:17:41 +0800

    remove unnecessary files

--- cmake-build-debug/CMakeFiles/dde_top_panel.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dde_top_panel.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/frame/CMakeFiles/dde-top-panel_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dde-top-panel_autogen"
-  "dde-top-panel_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dde-top-panel_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
